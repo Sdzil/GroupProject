24f15f2aaf337ca50a670a1815746f1d02081a0e
cwy104seung28 <68813383+cwy104seung28@users.noreply.github.com>
2020-10-11 17:46:13 +0800

    make MVC

--- BackEnd/routes/web.php
+++ BackEnd/routes/web.php
@@ -20,3 +20,38 @@ Auth::routes(['register' => false, 'reset' =>false, 'verify' =>false]);
 Route::get('/admin', 'HomeController@index')->name('home');
 
 
+Route::prefix('admin')->middleware(['auth'])->group(function(){
+
+    // //Banner管理
+    // Route::get('banners', 'BannerController@index');
+    // Route::get('banners/create', 'BannerController@create');
+    // Route::post('banners/store', 'BannerController@store');
+    // Route::get('banners/edit/{id}', 'BannerController@edit');
+    // Route::post('banners/update/{id}', 'BannerController@update');
+    // Route::get('banners/destroy/{id}', 'BannerController@destroy');
+
+    //產品管理
+    Route::get('products', 'ProductController@index');
+    Route::get('products/create', 'ProductController@create');
+    Route::post('products/store', 'ProductController@store');
+    Route::get('products/edit/{id}', 'ProductController@edit');
+    Route::post('products/update/{id}', 'ProductController@update');
+    Route::get('products/destroy/{id}', 'ProductController@destroy');
+
+    //產品類型管理
+    Route::get('products_type', 'ProductTypeController@index');
+    Route::get('products_type/create', 'ProductTypeController@create');
+    Route::post('products_type/store', 'ProductTypeController@store');
+    Route::get('products_type/edit/{id}', 'ProductTypeController@edit');
+    Route::post('products_type/update/{id}', 'ProductTypeController@update');
+    Route::get('products_type/destroy/{id}', 'ProductTypeController@destroy');
+
+    //聯絡表單管理
+    Route::get('contacts', 'ContactController@index');
+    Route::get('contacts/create', 'ContactController@create');
+    Route::post('contacts/store', 'ContactController@store');
+    Route::get('contacts/edit/{id}', 'ContactController@edit');
+    Route::post('contacts/update/{id}', 'ContactController@update');
+    Route::get('contacts/destroy/{id}', 'ContactController@destroy');
+
+});
